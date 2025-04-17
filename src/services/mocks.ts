2cafebee0c3c5ea862c08f79f66bd9c487a95a4b
phreemason <mr.omarmason@gmail.com>
2025-04-17 11:23:27 -0400

    lint for mocks

--- src/services/mocks.ts
+++ src/services/mocks.ts
@@ -1,216 +1,251 @@
+import { type Book } from "@/types/book";
 export const mockBookSearchResponse = {
-    "bookList": [
+    'bookList': [
         {
-            "api_id": "4214",
-            "api_source": "goodreads",
-            "bookUrl": "4214.Life_of_Pi",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1631251689i/4214._SY75_.jpg",
-            "title": "Life of Pi",
-            "publication_date": "2001-01-01",
-            "rating": 3.94,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "4214",
-                "edition_count": 434,
-                "ratings_count": 1703702,
-                "series": null,
-                "series_number": null,
-                "authors": [
-                    "Yann Martel"
+            'api_id': '4214',
+            'api_source': 'goodreads',
+            'bookUrl': '4214.Life_of_Pi',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1631251689i/4214._SY75_.jpg',
+            'title': 'Life of Pi',
+            'publication_date': '2001-01-01',
+            'rating': 3.94,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '4214',
+                'edition_count': 434,
+                'ratings_count': 1703702,
+                'series': null,
+                'series_number': null,
+                'authors': [
+                    'Yann Martel'
                 ]
             }
         },
         {
-            "api_id": "489732",
-            "api_source": "goodreads",
-            "bookUrl": "489732.The_Picture_of_Dorian_Gray",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1454087681i/489732._SY75_.jpg",
-            "title": "The Picture of Dorian Gray",
-            "publication_date": "1890-01-01",
-            "rating": 4.13,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "489732",
-                "edition_count": 12160,
-                "ratings_count": 1727875,
-                "series": null,
-                "series_number": null,
-                "authors": [
-                    "Oscar Wilde",
-                    "Robert Mighall"
+            'api_id': '489732',
+            'api_source': 'goodreads',
+            'bookUrl': '489732.The_Picture_of_Dorian_Gray',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1454087681i/489732._SY75_.jpg',
+            'title': 'The Picture of Dorian Gray',
+            'publication_date': '1890-01-01',
+            'rating': 4.13,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '489732',
+                'edition_count': 12160,
+                'ratings_count': 1727875,
+                'series': null,
+                'series_number': null,
+                'authors': [
+                    'Oscar Wilde',
+                    'Robert Mighall'
                 ]
             }
         },
         {
-            "api_id": "10917",
-            "api_source": "goodreads",
-            "bookUrl": "10917.My_Sister_s_Keeper",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1369504683i/10917._SY75_.jpg",
-            "title": "My Sister’s Keeper",
-            "publication_date": "2004-01-01",
-            "rating": 4.1,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "10917",
-                "edition_count": 200,
-                "ratings_count": 1255481,
-                "series": null,
-                "series_number": null,
-                "authors": [
-                    "Jodi Picoult"
+            'api_id': '10917',
+            'api_source': 'goodreads',
+            'bookUrl': '10917.My_Sister_s_Keeper',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1369504683i/10917._SY75_.jpg',
+            'title': 'My Sister’s Keeper',
+            'publication_date': '2004-01-01',
+            'rating': 4.1,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '10917',
+                'edition_count': 200,
+                'ratings_count': 1255481,
+                'series': null,
+                'series_number': null,
+                'authors': [
+                    'Jodi Picoult'
                 ]
             }
         },
         {
-            "api_id": "39074550",
-            "api_source": "goodreads",
-            "bookUrl": "39074550-humble-pi",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552885037i/39074550._SY75_.jpg",
-            "title": "Humble Pi: A Comedy of Maths Errors",
-            "publication_date": "2019-01-01",
-            "rating": 4.1,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "39074550",
-                "edition_count": 30,
-                "ratings_count": 20346,
-                "series": null,
-                "series_number": null,
-                "authors": [
-                    "Matt    Parker"
+            'api_id': '39074550',
+            'api_source': 'goodreads',
+            'bookUrl': '39074550-humble-pi',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552885037i/39074550._SY75_.jpg',
+            'title': 'Humble Pi: A Comedy of Maths Errors',
+            'publication_date': '2019-01-01',
+            'rating': 4.1,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '39074550',
+                'edition_count': 30,
+                'ratings_count': 20346,
+                'series': null,
+                'series_number': null,
+                'authors': [
+                    'Matt    Parker'
                 ]
             }
         },
         {
-            "api_id": "15839976",
-            "api_source": "goodreads",
-            "bookUrl": "15839976-red-rising",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1461354651i/15839976._SY75_.jpg",
-            "title": "Red Rising",
-            "publication_date": "2014-01-01",
-            "rating": 4.27,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "15839976",
-                "edition_count": 143,
-                "ratings_count": 593161,
-                "series": "Red Rising Saga,",
-                "series_number": 1,
-                "authors": [
-                    "Pierce Brown"
+            'api_id': '15839976',
+            'api_source': 'goodreads',
+            'bookUrl': '15839976-red-rising',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1461354651i/15839976._SY75_.jpg',
+            'title': 'Red Rising',
+            'publication_date': '2014-01-01',
+            'rating': 4.27,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '15839976',
+                'edition_count': 143,
+                'ratings_count': 593161,
+                'series': 'Red Rising Saga,',
+                'series_number': 1,
+                'authors': [
+                    'Pierce Brown'
                 ]
             }
         },
         {
-            "api_id": "25987394",
-            "api_source": "goodreads",
-            "bookUrl": "25987394-witch-is-when-it-all-began",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1446035852i/25987394._SY75_.jpg",
-            "title": "Witch Is When It All Began",
-            "publication_date": "2015-01-01",
-            "rating": 3.91,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "25987394",
-                "edition_count": 4,
-                "ratings_count": 19525,
-                "series": "A Witch P.I. Mystery,",
-                "series_number": 1,
-                "authors": [
-                    "Adele Abbott"
+            'api_id': '25987394',
+            'api_source': 'goodreads',
+            'bookUrl': '25987394-witch-is-when-it-all-began',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1446035852i/25987394._SY75_.jpg',
+            'title': 'Witch Is When It All Began',
+            'publication_date': '2015-01-01',
+            'rating': 3.91,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '25987394',
+                'edition_count': 4,
+                'ratings_count': 19525,
+                'series': 'A Witch P.I. Mystery,',
+                'series_number': 1,
+                'authors': [
+                    'Adele Abbott'
                 ]
             }
         },
         {
-            "api_id": "59912428",
-            "api_source": "goodreads",
-            "bookUrl": "59912428-mad-honey",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1642705453i/59912428._SY75_.jpg",
-            "title": "Mad Honey",
-            "publication_date": "2022-01-01",
-            "rating": 4.08,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "59912428",
-                "edition_count": 43,
-                "ratings_count": 523170,
-                "series": null,
-                "series_number": null,
-                "authors": [
-                    "Jodi Picoult",
-                    "Jennifer Finney Boylan"
+            'api_id': '59912428',
+            'api_source': 'goodreads',
+            'bookUrl': '59912428-mad-honey',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1642705453i/59912428._SY75_.jpg',
+            'title': 'Mad Honey',
+            'publication_date': '2022-01-01',
+            'rating': 4.08,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '59912428',
+                'edition_count': 43,
+                'ratings_count': 523170,
+                'series': null,
+                'series_number': null,
+                'authors': [
+                    'Jodi Picoult',
+                    'Jennifer Finney Boylan'
                 ]
             }
         },
         {
-            "api_id": "5043",
-            "api_source": "goodreads",
-            "bookUrl": "5043.The_Pillars_of_the_Earth",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1576956100i/5043._SY75_.jpg",
-            "title": "The Pillars of the Earth",
-            "publication_date": "1989-01-01",
-            "rating": 4.34,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "5043",
-                "edition_count": 178,
-                "ratings_count": 798830,
-                "series": "Kingsbridge,",
-                "series_number": 1,
-                "authors": [
-                    "Ken Follett"
+            'api_id': '5043',
+            'api_source': 'goodreads',
+            'bookUrl': '5043.The_Pillars_of_the_Earth',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1576956100i/5043._SY75_.jpg',
+            'title': 'The Pillars of the Earth',
+            'publication_date': '1989-01-01',
+            'rating': 4.34,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '5043',
+                'edition_count': 178,
+                'ratings_count': 798830,
+                'series': 'Kingsbridge,',
+                'series_number': 1,
+                'authors': [
+                    'Ken Follett'
                 ]
             }
         },
         {
-            "api_id": "59720691",
-            "api_source": "goodreads",
-            "bookUrl": "59720691-wake",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1654093744i/59720691._SY75_.jpg",
-            "title": "Wake",
-            "publication_date": "2022-01-01",
-            "rating": 3.99,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "59720691",
-                "edition_count": 26,
-                "ratings_count": 8393,
-                "series": "PI Lane Holland,",
-                "series_number": 1,
-                "authors": [
-                    "Shelley Burr"
+            'api_id': '59720691',
+            'api_source': 'goodreads',
+            'bookUrl': '59720691-wake',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1654093744i/59720691._SY75_.jpg',
+            'title': 'Wake',
+            'publication_date': '2022-01-01',
+            'rating': 3.99,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '59720691',
+                'edition_count': 26,
+                'ratings_count': 8393,
+                'series': 'PI Lane Holland,',
+                'series_number': 1,
+                'authors': [
+                    'Shelley Burr'
                 ]
             }
         },
         {
-            "api_id": "26126631",
-            "api_source": "goodreads",
-            "bookUrl": "26126631-witch-is-when-life-got-complicated",
-            "cover_image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1446036079i/26126631._SY75_.jpg",
-            "title": "Witch Is When Life Got Complicated",
-            "publication_date": "2015-01-01",
-            "rating": 4.16,
-            "source": "api",
-            "epub_url": "",
-            "metadata": {
-                "goodreads_id": "26126631",
-                "edition_count": 3,
-                "ratings_count": 8006,
-                "series": "A Witch P.I. Mystery,",
-                "series_number": 2,
-                "authors": [
-                    "Adele Abbott"
+            'api_id': '26126631',
+            'api_source': 'goodreads',
+            'bookUrl': '26126631-witch-is-when-life-got-complicated',
+            'cover_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1446036079i/26126631._SY75_.jpg',
+            'title': 'Witch Is When Life Got Complicated',
+            'publication_date': '2015-01-01',
+            'rating': 4.16,
+            'source': 'api',
+            'epub_url': '',
+            'metadata': {
+                'goodreads_id': '26126631',
+                'edition_count': 3,
+                'ratings_count': 8006,
+                'series': 'A Witch P.I. Mystery,',
+                'series_number': 2,
+                'authors': [
+                    'Adele Abbott'
                 ]
             }
         }
     ]
 }
+
+export const mockBookDataResponse: Omit<Book, 'id'> = {
+    api_id: '26126631-witch-is-when-life-got-complicated',
+    api_source: 'goodreads',
+    cover_image_url: 'https://picsum.photos/64/96?random=10',
+    created_at: new Date().toISOString(),
+    date_added: new Date().toISOString(),
+    description: '35000 feet high in the sky. The sound of a distant waterfall washes over the cabin. Stewards roll thier cart up and down the aisles. Complementary refreshments exchange hands and the scent of cup noodles floats through the air. I alternate between extending and bending my legs in a futile attempt to find comfort.',
+    edition: null,
+    epub_path: null,
+    epub_url: '',
+    format: 'physical',
+    genres: ['Sci-Fi', 'Fiction', 'Romance', 'Space Opera', 'Audiobook'],
+    has_user_edits: false,
+    isbn10: '9845638562',
+    isbn13: '7492545028425',
+    language: 'English',
+    metadata: {
+        extraction_method: 'html',
+        authors: [
+            { name: 'Foodie Juan' },
+            'Elvira Lasam'
+        ]
+    },
+    publication_date: '2025-04-13T04:49:23.548Z',
+    publisher: 'Penguin Random House',
+    rating: 4.13,
+    source: 'goodreads',
+    title: 'The Big Book of Love',
+    total_duration: null,
+    total_pages: 265,
+    updated_at: new Date().toISOString()
+};
+
