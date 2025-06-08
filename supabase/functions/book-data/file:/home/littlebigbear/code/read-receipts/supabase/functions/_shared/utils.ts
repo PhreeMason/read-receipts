c762d50a8db15d37941bddcb169d95e3bd7baf37
phreemason <mr.omarmason@gmail.com>
2025-06-08 15:49:27 -0400

    add utils.ts file to shared functions directory

---
+++ supabase/functions/book-data/file:/home/littlebigbear/code/read-receipts/supabase/functions/_shared/utils.ts
@@ -0,0 +1,9 @@
+export function generateUrl(query) {
+  return `https://www.goodreads.com/search?q=${encodeURIComponent(query)}`;
+}
+export const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS'
+};
